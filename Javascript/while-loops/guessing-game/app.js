203f0bb0df060b6c29f06679ced82902c94e7456
Jimcl6 <jedllorente@gmail.com>
2024-05-19 21:53:50 +0800

    fixed q command

--- Javascript/while-loops/guessing-game/app.js
+++ Javascript/while-loops/guessing-game/app.js
@@ -1,6 +1,8 @@
-let intMaximum = parseInt(prompt("Enter a maximum number:"));
+let intMaximum = parseInt(
+  prompt("Enter a maximum number: (number should not be higher than 10)")
+);
 
-while (!intMaximum) {
+while (!intMaximum || intMaximum > 10) {
   parseInt(prompt("Enter a valid number!"));
 }
 
@@ -8,8 +10,11 @@ const targetNum = Math.floor(Math.random() * intMaximum) + 1;
 console.log(targetNum);
 
 let userGuess = parseInt(prompt("Enter your guess here:"));
+let attempts = 1;
 
-while (userGuess !== targetNum) {
+while (pareInt(userGuess) !== targetNum) {
+  if (userGuess === 'q') : alert('You quitter!') break;
+  attempts++;
   if (userGuess > targetNum) {
     userGuess = parseInt(prompt("Too high guess again!"));
   } else {
@@ -17,4 +22,16 @@ while (userGuess !== targetNum) {
   }
 }
 
-alert(`YOU GUESSED ${userGuess}! YOU GOT IT! CONGRATS!!!`);
+if (attempts > 3) {
+  alert(
+    `It took you ${attempts} guesses, you guessed ${userGuess}. You guessed right. But, What a noob.`
+  );
+} else if (attempts <= 3 && attempts > 1) {
+  alert(
+    `It took you ${attempts} guesses. You guessed ${userGuess}. You guessed right!`
+  );
+} else {
+  alert(
+    `You guessed ${userGuess}. You got it! It took you ${attempts} guess. What a pro!`
+  );
+}
